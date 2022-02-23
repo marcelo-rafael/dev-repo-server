52c3b8ab2ac5c1f4abe8e2ff3333b8bae91ab337
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2022-02-23 17:22:40 -0300

    Backend dev-repo-server

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import { Router } from 'express'
+
+import auth from './middlewares/auth'
+
+import HelloController from './controllers/HelloController'
+import SessionsController from './controllers/SessionsController'
+import RepositoriesController from './controllers/RepositoriesController'
+import UsersController from './controllers/UsersController'
+
+const routes = new Router()
+
+routes.post('/sessions', SessionsController.create)
+routes.get('/hello', HelloController.index)
+
+routes.use(auth)
+
+routes.get('/users', UsersController.index)
+routes.get('/users/:id', UsersController.show)
+routes.post('/users', UsersController.create)
+routes.put('/users/:id', UsersController.update)
+routes.delete('/users/:id', UsersController.destroy)
+
+routes.get('/users/:user_id/repositories', RepositoriesController.index)
+routes.post('/users/:user_id/repositories', RepositoriesController.create)
+routes.delete('/users/:user_id/repositories', RepositoriesController.destroy)
+
+export default routes
